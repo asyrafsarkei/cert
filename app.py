9c6e5361f935eb93ea727e152b30399d879d35e5
asyrafsarkei <muhamadasyraf.sarkei@gmail.com>
2025-12-26 15:31:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -125,6 +125,5 @@ def logout():
     return redirect(url_for('index'))
 
 # Run app
-gunicorn app:app
 # if __name__ == '__main__':
 #    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
